4e0e420f76077b5cb9dd6be0ff730f1217f6888f
Yadav <avaya@147dda0da62d.ant.amazon.com>
2020-10-21 21:22:02 +0530

    Setting up our React app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+        REGION: 'us-east-1',
+        BUCKET: 'notes-upload-bucket-avaya'
+    },
+    apiGateway: {
+        REGION: 'us-east-1',
+        URL: 'https://u7gsv7f5d8.execute-api.us-east-1.amazonaws.com/prod'
+    },
+    cognito: {
+        REGION: 'us-east-1',
+        USER_POOL_ID: 'us-east-1_f4cjIyZQY',
+        APP_CLIENT_ID: '613les12u1pf5pvt8m2buietnc',
+        IDENTITY_POOL_ID: 'us-east-1:7af7aac4-9505-406a-92c4-b2aa04d3479d'
+    }
+};
